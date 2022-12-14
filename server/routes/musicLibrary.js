cfb364f03f5049a9c6efca29434d2194b68bed3b
Linssi Colin Ruiz <linssi101@gmail.com>
2022-12-14 17:34:26 -0600

    added a get id and delete router

--- server/routes/musicLibrary.js
+++ server/routes/musicLibrary.js
@@ -15,14 +15,54 @@ router.get("/", (req, res) => {
 		});
 });
 
+// router.get('/artist', (req, res) => {
+// 	`SELECT * FROM songs WHERE artist = ${req.params.id}`
+// });
+
+// to test and see if route is set up correctly
+router.get("/:id", (req, res) => {
+	console.log("hello from get request!", req.params.id);
+	const queryText = `SELECT # FROM songs WHERE id = ${req.params.id};`;
+	pool.query(queryText)
+		.then((result) => {
+			console.log("results from DB", result);
+			res.send(result.rows);
+		})
+		.catch((error) => {
+			console.log("error making a query", error);
+			res.sendStatus(500);
+		});
+});
+
+router.delete("/:id", (req, res) => {
+	console.log("hello from delete request!", req.params.id);
+	const queryText = `DELETE from songs WHERE id = ${req.params.id};`;
+	pool.query(queryText)
+		.then((result) => {
+			console.log(result);
+			res.send(202);
+		})
+		.catch((error) => {
+			console.log("error making a query", error);
+			res.sendStatus(500);
+		});
+});
+
 router.post("/", (req, res) => {
 	const newSong = req.body;
 	const queryText = `  
     INSERT INTO "songs" ("rank", "artist", "track", "published")
-    VALUES (${newSong.rank}, '${newSong.artist}', '${newSong.track}', '${newSong.published}');
+    VALUES ($1, $2, $3, $4);
     `;
+
+	// ${newSong.rank}, '${newSong.artist}', '${newSong.track}', '${newSong.published}'
 	pool
-		.query(queryText)
+		.query(queryText, [
+			newSong.rank,
+			newSong.artist,
+			newSong.track,
+			newSong.published,
+		])
 		.then((result) => {
 			console.log("result", result);
 			res.sendStatus(201);
