f249a65b5ef3e37818e8ccdbfc7f9a230c34920b
Linssi Colin Ruiz <linssi101@gmail.com>
2022-12-14 18:53:38 -0600

    created delete router

--- server/routes/musicLibrary.js
+++ server/routes/musicLibrary.js
@@ -20,32 +20,34 @@ router.get("/", (req, res) => {
 // });
 
 // to test and see if route is set up correctly
-router.get("/:id", (req, res) => {
-	console.log("hello from get request!", req.params.id);
-	const queryText = `SELECT # FROM songs WHERE id = ${req.params.id};`;
-	pool.query(queryText)
-		.then((result) => {
-			console.log("results from DB", result);
-			res.send(result.rows);
-		})
-		.catch((error) => {
-			console.log("error making a query", error);
-			res.sendStatus(500);
-		});
+router.get('/:id', (req, res) => {
+    console.log("Hello from get request!", req.params.id);
+    const queryText = `SELECT * from songs WHERE id = ${req.params.id};`;
+    pool.query(queryText)
+    .then((result) => {
+        console.log('results from DB', result);
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log('error making a query', error);
+        res.sendStatus(500);
+    });
 });
 
-router.delete("/:id", (req, res) => {
-	console.log("hello from delete request!", req.params.id);
-	const queryText = `DELETE from songs WHERE id = ${req.params.id};`;
-	pool.query(queryText)
-		.then((result) => {
-			console.log(result);
-			res.send(202);
-		})
-		.catch((error) => {
-			console.log("error making a query", error);
-			res.sendStatus(500);
-		});
+
+
+router.delete('/:id', (req, res) => {
+    console.log("Hello from delete request!", req.params.id);
+    const queryText = `DELETE from songs WHERE id = ${req.params.id};`;
+    pool.query(queryText)
+    .then((result) => {
+        console.log(result);
+        res.sendStatus(202);
+    })
+    .catch((error) => {
+        console.log('error making a query', error);
+        res.sendStatus(500);
+    });
 });
 
 router.post("/", (req, res) => {
