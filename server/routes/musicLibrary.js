c6cb36e9dc9e1772aaa9be2e5258950a15908256
Linssi Colin Ruiz <linssi101@gmail.com>
2022-12-14 19:10:34 -0600

    created put route

--- server/routes/musicLibrary.js
+++ server/routes/musicLibrary.js
@@ -15,49 +15,45 @@ router.get("/", (req, res) => {
 		});
 });
 
-// router.get('/artist', (req, res) => {
-// 	`SELECT * FROM songs WHERE artist = ${req.params.id}`
-// });
-
 // to test and see if route is set up correctly
-router.get('/:id', (req, res) => {
-    console.log("Hello from get request!", req.params.id);
-    const queryText = `SELECT * from songs WHERE id = ${req.params.id};`;
-    pool.query(queryText)
-    .then((result) => {
-        console.log('results from DB', result);
-        res.send(result.rows);
-    })
-    .catch((error) => {
-        console.log('error making a query', error);
-        res.sendStatus(500);
-    });
+router.get("/:id", (req, res) => {
+	console.log("Hello from get request!", req.params.id);
+	const queryText = `SELECT * from songs WHERE id = ${req.params.id};`;
+	pool
+		.query(queryText)
+		.then((result) => {
+			console.log("results from DB", result);
+			res.send(result.rows);
+		})
+		.catch((error) => {
+			console.log("error making a query", error);
+			res.sendStatus(500);
+		});
 });
 
-
-
-router.delete('/:id', (req, res) => {
-    console.log("Hello from delete request!", req.params.id);
-    const queryText = `DELETE from songs WHERE id = ${req.params.id};`;
-    pool.query(queryText)
-    .then((result) => {
-        console.log(result);
-        res.sendStatus(202);
-    })
-    .catch((error) => {
-        console.log('error making a query', error);
-        res.sendStatus(500);
-    });
+router.delete("/:id", (req, res) => {
+	console.log("Hello from delete request!", req.params.id);
+	const queryText = `DELETE from songs WHERE id = ${req.params.id};`;
+	pool
+		.query(queryText)
+		.then((result) => {
+			console.log(result);
+			res.sendStatus(202);
+		})
+		.catch((error) => {
+			console.log("error making a query", error);
+			res.sendStatus(500);
+		});
 });
 
 router.post("/", (req, res) => {
 	const newSong = req.body;
+	// ${newSong.rank}, '${newSong.artist}', '${newSong.track}', '${newSong.published}'
 	const queryText = `  
     INSERT INTO "songs" ("rank", "artist", "track", "published")
     VALUES ($1, $2, $3, $4);
     `;
 
-	// ${newSong.rank}, '${newSong.artist}', '${newSong.track}', '${newSong.published}'
 	pool
 		.query(queryText, [
 			newSong.rank,
@@ -73,6 +69,12 @@ router.post("/", (req, res) => {
 			console.log("error making insert query", error);
 			res.sendStatus(500);
 		});
+});
+
+router.put("/rank/:id", (req, res) => {
+	console.log("rank id", req.params.id);
+	console.log("rank body", req.body);
+	res.sendStatus(200);
 });
 
 module.exports = router;
